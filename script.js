6a765b2e23fd06d8774af62b6002f77091b13a1a
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2024-01-17 10:54:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-// complete this js code
-function Person(name, age) {
-	Person.prototype.greet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`);
-	}
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
 }
 
+Person.prototype.greet = function () {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`);
+};
+
 function Employee(name, age, jobTitle) {
-	function Employee extends Person{
-		Employee.prortotype.jobGreet()
-		{
-			console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
-		}
-	}
+    Person.call(this, name, age); // Call the Person constructor with the Employee context
+    this.jobTitle = jobTitle;
 }
+
+// Inherit the Person prototype
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function () {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+};
 
 // Do not change code below this line
 window.Person = Person;
