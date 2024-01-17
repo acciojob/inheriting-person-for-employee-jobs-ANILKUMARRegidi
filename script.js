8a4e94a8314de624861ff7bb20912c36adaf2b46
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2024-01-17 10:59:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
- function Person(name, age) {
+ // Person constructor function
+function Person(name, age) {
     this.name = name;
     this.age = age;
 }
 
+// Greet method for Person prototype
 Person.prototype.greet = function () {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
+// Employee constructor function inheriting from Person
 function Employee(name, age, jobTitle) {
-    Person.call(this, name, age); // Call the Person constructor with the Employee context
+    Person.call(this, name, age);
     this.jobTitle = jobTitle;
 }
 
@@ -16,6 +19,7 @@ function Employee(name, age, jobTitle) {
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
+// JobGreet method for Employee prototype
 Employee.prototype.jobGreet = function () {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
